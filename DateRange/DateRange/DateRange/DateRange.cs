24eede0382689d12cf76849380f3b9de44fe02cd
= <=>
2021-06-30 00:31:25 +0100

    added new code

---
+++ DateRange/DateRange/DateRange/DateRange.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JO.DateRange
+{
+    public enum DateIncrement
+    {
+        minutes,
+        days,
+        weeks,
+        months,
+        years
+    }
+
+    public class DateRange
+    {
+
+        private DateTime _startDate;
+        private DateTime _endDate;
+
+        private List<DayOfWeek> _excludeDaysOfWeek;
+        private List<int> _excludeDays;
+        private List<DayOfWeek> _includeDaysOfWeek;
+        private List<int> _includeDays;
+        private string _dailyOrWeekly;
+
+        public DateRange(DateTime startDate, DateTime endDate, string DailyOrWeekly = "daily")
+        {
+            init(startDate, endDate, DailyOrWeekly);
+        }
+
+        public DateRange(DateTime startDate, int incrementAmount, DateIncrement dateIncrement, string DailyOrWeekly = "daily")
+        {
+            DateTime endDate = startDate;
+
+            switch(dateIncrement)
+            {
+                case DateIncrement.minutes:
+                    endDate = startDate.AddMinutes(incrementAmount);
+                    break;
+                case DateIncrement.days:
+                    endDate = startDate.AddDays(incrementAmount);
+                    break;
+                case DateIncrement.weeks:
+                    incrementAmount = incrementAmount * 7;
+                    endDate = startDate.AddDays(incrementAmount);
+                    break;
+                case DateIncrement.months:
+                    endDate = startDate.AddMonths(incrementAmount);
+                    break;
+            }
+
+            init(startDate, endDate, DailyOrWeekly);
+        }
+
+        private void init(DateTime startDate, DateTime endDate, string DailyOrWeekly = "daily")
+        {
+            _startDate = startDate;
+            _endDate = endDate;
+            _excludeDaysOfWeek = new List<DayOfWeek>();
+            _excludeDays = new List<int>();
+
+            _includeDaysOfWeek = new List<DayOfWeek>();
+            _includeDays = new List<int>();
+
+            _dailyOrWeekly = (DailyOrWeekly != string.Empty) ? DailyOrWeekly.ToLower().Trim() : "daily";
+        }
+
+        public DateRange IgnoreDaysOfWeek(DayOfWeek dayOfWeek)
+        {
+            _excludeDaysOfWeek.Add(dayOfWeek);
+
+            return this;
+        }
+
+        public DateRange IgnoreDaysOfWeekRange(List<DayOfWeek> daysOfWeek)
+        {
+            _excludeDaysOfWeek.AddRange(daysOfWeek);
+
+            return this;
+        }
+
+        public DateRange IgnoreDay(int day)
+        {
+            _excludeDays.Add(day);
+
+            return this;
+        }
+
+        public DateRange IgnoreDays(List<int> days)
+        {
+            _excludeDays.AddRange(days);
+
+            return this;
+        }
+
+        public DateRange IncludeDayOfWeekRange(List<DayOfWeek> daysOfWeek)
+        {
+            _includeDaysOfWeek.AddRange(daysOfWeek);
+
+            return this;
+        }
+
+        public DateRange IncludeDayOfWeek(DayOfWeek dayOfWeek)
+        {
+            _includeDaysOfWeek.Add(dayOfWeek);
+
+            return this;
+        }
+
+        public DateRange IncludeDaysRange(List<int> days)
+        {
+            _includeDays.AddRange(days);
+
+            return this;
+        }
+
+        public DateRange IncludeDays(int days)
+        {
+            _includeDays.Add(days);
+
+            return this;
+        }
+
+        private List<DateTime> createDates()
+        {
+            List<DateTime> listOfDates = new List<DateTime>();
+            TimeSpan ts = _endDate - _startDate;
+
+            int incrementDays = 1;
+
+            switch (_dailyOrWeekly)
+            {
+                case "daily":
+                    incrementDays = 1;
+                    break;
+                case "weekly":
+                    incrementDays = 7;
+                    break;
+                default:
+                    incrementDays = 1;
+                    break;
+            }
+
+            int totalDays = ts.Days;
+
+            DateTime currentDate = _startDate;
+            bool ignoreFlag = false;
+
+            while (currentDate.Date <= _endDate.Date)
+            {
+                // RUN LOGIC CHECK RULES
+                if (_excludeDaysOfWeek.Contains(currentDate.DayOfWeek))
+                {
+                    ignoreFlag = true;
+                }
+
+                if (_excludeDays.Contains(currentDate.Day))
+                {
+                    ignoreFlag = true;
+                }
+
+                if (!ignoreFlag)
+                {
+                    if (_includeDaysOfWeek.Count > 0)
+                    {
+                        ignoreFlag = !(_includeDaysOfWeek.Contains(currentDate.DayOfWeek));
+                    }
+
+
+                    if (_includeDays.Count > 0)
+                    {
+                        ignoreFlag = !(_includeDays.Contains(currentDate.Day));
+                    }
+                }
+
+                if (!ignoreFlag)
+                {
+                    listOfDates.Add(currentDate);
+                }
+
+                currentDate = currentDate.AddDays(incrementDays);
+                ignoreFlag = false;
+            }
+
+            return listOfDates;
+        }
+
+        public List<DateTime> ToList()
+        {
+            List<DateTime> listOfDates = createDates();
+
+            return listOfDates;
+        }
+
+
+        public Dictionary<string, List<DateTime>> ToDictionaryGroupByMonth()
+        {
+            Dictionary<string, List<DateTime>> listOfDates = new Dictionary<string, List<DateTime>>();
+            List<DateTime> tempList = new List<DateTime>();
+
+            tempList = createDates();
+
+            List<string> months = new List<string>()
+            {
+                "", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+            };
+
+            var q = (from d in tempList
+                     group d by months[d.Month] into g
+                     select g);
+
+            listOfDates = q.ToDictionary(x => x.Key, x => x.ToList());
+
+            return listOfDates;
+        }
+
+        public Dictionary<string, List<DateTime>> ToDictionaryGroupByDayOfWeek()
+        {
+            Dictionary<string, List<DateTime>> listOfDates = new Dictionary<string, List<DateTime>>();
+            List<DateTime> tempList = new List<DateTime>();
+
+            tempList = createDates();
+
+            var q = (from d in tempList
+                     group d by d.DayOfWeek.ToString() into g
+                     select g);
+
+            listOfDates = q.ToDictionary(x => x.Key, x => x.ToList());
+
+            return listOfDates;
+        }
+
+
+
+    }
+}
---
+++ DateRange/TestConsole/TestConsole/Program.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JO.DateRange;
+
+namespace TestConsole
+{
+    class Program
+    {
+        private static DateTime _startDate = new DateTime();
+        private static DateTime _endDate = new DateTime();
+        private static List<DayOfWeek> _excludedaysOfWeek = new List<DayOfWeek>();
+        private static List<DayOfWeek> _includedaysOfWeek = new List<DayOfWeek>();
+        private static List<int> _excludeDays = new List<int>();
+        private static List<int> _includeDays = new List<int>();
+
+        static void Main(string[] args)
+        {
+            // TEST BASIC RANGE OF DATES
+            //getBasicRangeOfDates();
+
+            //getFilterIncludeDaysOfWeek();
+
+            //getFilterExcludeDaysOfWeek();
+
+            //getGroupDates();
+
+            //getGroupDatesFiltered();
+
+            //getGroupDatesByDayOfWeek();
+
+            //testChainDates();
+
+            createDateIncrement();
+
+            Console.ReadLine();
+        }
+
+        private static void resetData()
+        {
+            DateTime tempDate = DateTime.Now;
+            _startDate = new DateTime(tempDate.Year, tempDate.Month, 1, 0, 0, 0);
+            _endDate = _startDate.AddMonths(1).AddDays(-1);
+            _excludedaysOfWeek = new List<DayOfWeek>();
+            _includedaysOfWeek = new List<DayOfWeek>();
+            _excludeDays = new List<int>();
+            _includeDays = new List<int>();
+        }
+
+
+
+        private static void getBasicRangeOfDates()
+        {
+            resetData();
+
+            Console.WriteLine("Daily Range of Dates");
+            getDates(_startDate, _endDate, false, "daily", _excludedaysOfWeek, _excludeDays, _includedaysOfWeek, _includeDays, string.Empty);
+
+            Console.WriteLine("Weekly Range of Dates");
+            getDates(_startDate, _endDate, false, "weekly", _excludedaysOfWeek, _excludeDays, _includedaysOfWeek, _includeDays, string.Empty);
+        }
+
+        private static void getFilterIncludeDaysOfWeek()
+        {
+            resetData();
+
+            _includedaysOfWeek.Add(DayOfWeek.Tuesday);
+            _includedaysOfWeek.Add(DayOfWeek.Thursday);
+
+            Console.WriteLine("Weekly Range of Dates - Should only show {0}", string.Join(",", _includedaysOfWeek.ToArray()));
+            getDates(_startDate, _endDate, false, "daily", _excludedaysOfWeek, _excludeDays, _includedaysOfWeek, _includeDays, string.Empty);
+        }
+
+        private static void getFilterExcludeDaysOfWeek()
+        {
+            resetData();
+
+            _excludedaysOfWeek.Add(DayOfWeek.Tuesday);
+            _excludedaysOfWeek.Add(DayOfWeek.Thursday);
+
+            Console.WriteLine("Weekly Range of Dates - Should exclude {0}", string.Join(",", _excludedaysOfWeek.ToArray()));
+            getDates(_startDate, _endDate, false, "daily", _excludedaysOfWeek, _excludeDays, _includedaysOfWeek, _includeDays, string.Empty);
+        }
+
+        private static void getGroupDates()
+        {
+            resetData();
+
+            DateTime tempDate = DateTime.Now;
+            _startDate = new DateTime(tempDate.Year, tempDate.Month, 1, 0, 0, 0);
+            _endDate = _startDate.AddYears(1);
+
+            Console.WriteLine("Group Range of Dates");
+            getDates(_startDate, _endDate, true, "daily", _excludedaysOfWeek, _excludeDays, _includedaysOfWeek, _includeDays, string.Empty);
+        }
+
+        private static void getGroupDatesFiltered()
+        {
+            resetData();
+
+            DateTime tempDate = DateTime.Now;
+            _startDate = new DateTime(tempDate.Year, tempDate.Month, 1, 0, 0, 0);
+            _endDate = _startDate.AddYears(1);
+
+            _includedaysOfWeek.Add(DayOfWeek.Monday);
+            _includedaysOfWeek.Add(DayOfWeek.Tuesday);
+
+            Console.WriteLine("Group Range of Dates - Filter by {0}", string.Join(",", _includedaysOfWeek.ToArray()));
+            getDates(_startDate, _endDate, true, "daily", _excludedaysOfWeek, _excludeDays, _includedaysOfWeek, _includeDays, string.Empty);
+        }
+
+        private static void getGroupDatesByDayOfWeek()
+        {
+            resetData();
+
+            DateTime tempDate = DateTime.Now;
+            _startDate = new DateTime(tempDate.Year, tempDate.Month, 1, 0, 0, 0);
+            _endDate = _startDate.AddYears(1);
+
+            Console.WriteLine("Group Range of Dates by Day of Week");
+            getDates(_startDate, _endDate, true, "daily", _excludedaysOfWeek, _excludeDays, _includedaysOfWeek, _includeDays, "daysofweek");
+        }
+
+        private static void testChainDates()
+        {
+            resetData();
+
+            Console.WriteLine("Daily Range of Dates");
+            dateChain(_startDate, _endDate);
+
+        }
+
+        private static void createDateIncrement()
+        {
+            resetData();
+
+            var tempDate = DateTime.Now;
+            _startDate = new DateTime(tempDate.Year, tempDate.Month, 1, 0, 0, 0);
+
+            DateRange dateRange = new DateRange(_startDate, 3, DateIncrement.months, "daily");
+
+            var listOfDates = dateRange.ToList();
+
+            Console.WriteLine("Start Date: {0} {1}", _startDate.DayOfWeek, _startDate.ToLongDateString());
+
+            foreach (var d in listOfDates)
+            {
+                Console.WriteLine("{0} {1}", d.DayOfWeek, d.ToLongDateString());
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+        }
+
+        static void getDates(DateTime startDate, DateTime endDate,
+            bool groupDates,
+            string dailyOrWeekly,
+            List<DayOfWeek> ignoreDaysOfWeek,
+            List<int> ignoreDays,
+            List<DayOfWeek> includeDaysOfWeek,
+            List<int> includeDays,
+            string groupType
+            )
+        {
+            DateRange dateRange = new DateRange(startDate, endDate, dailyOrWeekly);
+
+            if (ignoreDaysOfWeek != null)
+            {
+                dateRange.IgnoreDaysOfWeekRange(ignoreDaysOfWeek);
+            }
+
+            if (ignoreDays != null)
+            {
+                dateRange.IgnoreDays(ignoreDays);
+            }
+
+            if (includeDays != null)
+            {
+                dateRange.IncludeDaysRange(includeDays);
+            }
+
+            if (includeDaysOfWeek != null)
+            {
+                dateRange.IncludeDayOfWeekRange(includeDaysOfWeek);
+            }
+
+            Console.WriteLine("Start Date: {0} {1}", startDate.DayOfWeek, startDate.ToLongDateString());
+            Console.WriteLine("End Date: {0} {1}", endDate.DayOfWeek, endDate.ToLongDateString());
+
+            if (groupDates)
+            {
+                Console.WriteLine("Group by Dates");
+                Console.WriteLine("=========================\r\n");
+
+                Dictionary<string, List<DateTime>> groupOfDates = new Dictionary<string, List<DateTime>>();
+
+                if (groupType == "daysofweek")
+                {
+                    groupOfDates = dateRange.ToDictionaryGroupByDayOfWeek();
+                } else
+                {
+                    groupOfDates = dateRange.ToDictionaryGroupByMonth();
+                }
+
+                foreach (var group in groupOfDates)
+                {
+                    Console.WriteLine("{0}", group.Key);
+
+                    foreach (var d in group.Value)
+                    {
+                        Console.WriteLine("{0} {1}", d.DayOfWeek, d.ToLongDateString());
+                    }
+
+                    Console.WriteLine("");
+                }
+            }
+            else
+            {
+                var listOfDates = dateRange.ToList();
+
+                foreach (var d in listOfDates)
+                {
+                    Console.WriteLine("{0} {1}", d.DayOfWeek, d.ToLongDateString());
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+        }
+
+        /// <summary>
+        /// Test chain function
+        /// </summary>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        static void dateChain(DateTime startDate, DateTime endDate)
+        {
+            DateRange dateRange = new DateRange(startDate, endDate, "daily");
+
+            Console.WriteLine("Start Date: {0} {1}", startDate.DayOfWeek, startDate.ToLongDateString());
+            Console.WriteLine("End Date: {0} {1}", endDate.DayOfWeek, endDate.ToLongDateString());
+
+            var listOfDates = dateRange.IgnoreDaysOfWeek(DayOfWeek.Monday)
+                    .IgnoreDaysOfWeek(DayOfWeek.Friday)
+                    .ToList();
+
+            foreach (var d in listOfDates)
+            {
+                Console.WriteLine("{0} {1}", d.DayOfWeek, d.ToLongDateString());
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+        }
+
+
+    }
+}
